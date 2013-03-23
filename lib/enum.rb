b193f518a1f043df3ff13c090f61b5a7cecfe0af
Oded Niv <oded.niv@gmail.com>
2013-03-23 03:45:08 +0200

    locate EnumValue by to_s of the object given

--- lib/enum.rb
+++ lib/enum.rb
@@ -3,7 +3,7 @@ class Enum
   include Enumerable
 
   attr_reader :klass, :name, :by_name, :by_value
-  
+
   def initialize(name, klass=nil, hash={})
     klass, hash = nil, klass if klass.is_a?(Hash)
     @name, @klass = name, klass
@@ -32,7 +32,7 @@ class Enum
     return name_or_values.map { |name_or_value| self[name_or_value] } if name_or_values.length > 1
 
     name_or_value = name_or_values.first
-    ev = @by_name[name_or_value] || @by_value[name_or_value]
+    ev = @by_name_s[name_or_value.to_s] || @by_value_s[name_or_value.to_s]
     raise "#{inspect} does not know #{name_or_value.inspect}" if ev.nil?
     ev
   end
@@ -41,12 +41,14 @@ class Enum
   def map_hash(hash)
     @by_name = {}
     @by_value = {}
+    @by_name_s = {}
+    @by_value_s = {}
     hash.each do |n, v|
       n, v = v, n if v.is_a?(Symbol) or n.is_a?(Numeric)
       raise "duplicate enum name #{n} for #{to_s}" if @by_name.has_key?(n)
       raise "duplicate enum value #{v} for #{to_s}.#{n}" if @by_value.has_key?(v)
       raise "value can't be nil for #{to_s}.#{n}" if v.nil?
-      @by_name[n] = @by_value[v] = EnumValue.new(self, n, v)
+      @by_name_s[n.to_s] = @by_value_s[v.to_s] = @by_name[n] = @by_value[v] = EnumValue.new(self, n, v)
     end
     @by_name.freeze
     @by_value.freeze
